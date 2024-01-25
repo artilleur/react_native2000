1cf423333ed0be8a9189c0f77d5c5266ef537a20
artilleur <99259153+artilleur@users.noreply.github.com>
2024-01-25 12:49:12 +0100

    25012024

--- Calculator.js
+++ Calculator.js
@@ -5,32 +5,29 @@ const Calculator = () => {
   const [expression, setExpression] = useState('');
 
   const handleButtonPress = (value) => {
-    if (value === '%') {
-      setExpression((prevExpression) => {
-        const lastChar = prevExpression.slice(-1);
-        if (!isNaN(lastChar) || lastChar === ')') {
-          const lastNumberMatch = prevExpression.match(/(\d*\.?\d+)$/);
-          if (lastNumberMatch) {
-            const lastNumber = lastNumberMatch[0];
-            const updatedExpression =
-  prevExpression.slice(0, -lastNumber.length) + lastNumber + '/100*' + prevExpression.slice(0, -lastNumber.length);
-// Supprimer le "+" à la fin s'il y en a un
-return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : updatedExpression;
-          }
-        } 
-
-        return prevExpression;
-      });
-    } else {
-      setExpression((prevExpression) => {
-        const lastChar = prevExpression.slice(-1);
-        // Pour éviter d'ajouter deux signes "+" consécutifs
-        if (value === '+' && (isNaN(lastChar) || lastChar === '+')) {
-          return prevExpression;
-        }
+    setExpression((prevExpression) => {
+      const lastChar = prevExpression.slice(-1);
+  
+      if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '(') {
+        // Si le dernier caractère est un opérateur, ajoutez le nombre directement
         return prevExpression + value;
-      });
-    }
+      } else if (!isNaN(lastChar) || lastChar === ')') {
+        const lastNumberMatch = prevExpression.match(/(\d*\.?\d+)$/);
+        if (lastNumberMatch) {
+          const lastNumber = lastNumberMatch[0];
+          const updatedExpression =
+            prevExpression.slice(0, -lastNumber.length) + lastNumber + value;
+          return updatedExpression.endsWith('-') ? updatedExpression.slice(0, -1) : updatedExpression;
+        }
+      }
+  
+      // Ajouter l'opérateur seulement si la dernière expression n'est pas un opérateur
+      if (!isNaN(lastChar) || lastChar === '.') {
+        return prevExpression + value;
+      }
+  
+      return prevExpression;
+    });
   };
 
   const handleClear = () => {
@@ -51,8 +48,6 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
     setExpression((prevExpression) => prevExpression.slice(0, -1));
   };
 
-
-
   return (
     <View style={styles.container}>
       <View style={styles.display}>
@@ -60,12 +55,12 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
       </View>
       <View style={styles.buttonsContainer}>
         <View style={styles.row}>
-          
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('8')}>
-            <Text style={styles.buttonText}>8</Text>
+         
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('*')}>
+            <Text style={styles.buttonText3}>*</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('9')}>
-            <Text style={styles.buttonText}>9</Text>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('-')}>
+            <Text style={styles.buttonText3}>-</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('/')}>
             <Text style={styles.buttonText3}>/</Text>
@@ -73,12 +68,13 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('(')}>
             <Text style={styles.buttonText3}>(</Text>
           </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress(')')}>
+            <Text style={styles.buttonText3}>)</Text>
+          </TouchableOpacity>
         </View>
         {/* ... Ajoutez d'autres rangées en fonction de vos besoins */}
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('4')}>
-            <Text style={styles.buttonText}>4</Text>
-          </TouchableOpacity>
+          
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('5')}>
             <Text style={styles.buttonText}>5</Text>
           </TouchableOpacity>
@@ -88,16 +84,18 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('7')}>
             <Text style={styles.buttonText}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('*')}>
-            <Text style={styles.buttonText3}>*</Text>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('8')}>
+            <Text style={styles.buttonText}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress(')')}>
-            <Text style={styles.buttonText3}>)</Text>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('9')}>
+            <Text style={styles.buttonText}>9</Text>
           </TouchableOpacity>
+          
+          
         </View>
         {/* ... Ajoutez d'autres rangées en fonction de vos besoins */}
         <View style={styles.row}>
-        <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('0')}>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('0')}>
             <Text style={styles.buttonText}>0</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('1')}>
@@ -109,31 +107,26 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('3')}>
             <Text style={styles.buttonText}>3</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('-')}>
-            <Text style={styles.buttonText3}>-</Text>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('4')}>
+            <Text style={styles.buttonText}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('+')}>
-            <Text style={styles.buttonText3}>+</Text>
-          </TouchableOpacity>
+          
+          
         </View>
         <View style={styles.row}>
-          
           <TouchableOpacity style={styles.button} onPress={() => handleClear()}>
             <Text style={styles.buttonText2}>C</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={() => handleDelete()}>
             <Text style={styles.buttonText2}>del</Text>
           </TouchableOpacity>
-         
-          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('%')}>
-            <Text style={styles.buttonText3}>%</Text>
-          </TouchableOpacity>
-          
-         
           <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('.')}>
-          <Text style={styles.buttonText3}>.</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleEvaluate()}>
+            <Text style={styles.buttonText3}>.</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => handleButtonPress('+')}>
+            <Text style={styles.buttonText3}>+</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => handleEvaluate()}>
             <Text style={styles.buttonText3}>=</Text>
           </TouchableOpacity>
         </View>
@@ -142,15 +135,11 @@ return updatedExpression.endsWith('+') ? updatedExpression.slice(0, -1) : update
   );
 };
 
-// ...
-
-
-// ...
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', // Centrer verticalement
-    alignItems: 'center', // Centrer horizontalement
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   display: {
     backgroundColor: 'yellows',
@@ -162,8 +151,8 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     backgroundColor: '#ffdead',
-    width: '80%', // Ajuster la largeur de la calculatrice selon vos besoins
-    maxWidth: 400, // Limiter la largeur maximale de la calculatrice
+    width: '80%',
+    maxWidth: 400,
   },
   row: {
     flexDirection: 'row',
@@ -178,20 +167,16 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 20,
-    color : 'black',
-   
+    color: 'black',
   },
   buttonText2: {
     fontSize: 15,
-    color:'red',
+    color: 'red',
   },
   buttonText3: {
     fontSize: 30,
-    color : '#ff8c00',
-   
+    color: '#ff8c00',
   },
 });
-// ...
-
 
 export default Calculator;
